545ef0cf4ab396931e6d8dfdda6e65f3a3c91d42
Undeadender <78551700+Undeadender@users.noreply.github.com>
2026-02-10 18:56:48 -0500

    onepage start

--- links/main.js
+++ links/main.js
@@ -1,3 +1,9 @@
+var gunActive = false;
+var spotActive = false;
+var secActive = false;
+var logiActive = false;
+
+
 function navFunction() {
   var x = document.getElementById("nav-sec");
   var y = document.getElementById("drive-sec");
@@ -18,6 +24,67 @@ function driveFunction() {
   } else {
     y.style.display = "block";
     x.style.display = "none";
+  }
+}
+
+function showroleinfo(roleSec, driveNavSec) {
+  var a = document.getElementById(roleSec);
+  var b = document.getElementById("drive-sec");
+  var c = document.getElementById("nav-sec");
+  var x = document.getElementById("whale-sec");
+  var y = document.getElementById("sail-sec");
+  
+  if (roleSec === "spot-sec" || roleSec === "gun-sec") {
+    y.style.display = "none";
+    x.style.display = "block";
+  } else {
+    y.style.display = "block";
+    x.style.display = "none";
+  }
+  if (driveNavSec === "nav-sec") {
+    b.style.display = "none";
+    c.style.display = "block";
+  } else if (driveNavSec === "drive-sec") {
+    b.style.display = "block";
+    c.style.display = "none";
+  } else {
+    b.style.display = "none";
+    c.style.display = "none";
+  }
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
+
+function showall() {
+  var x = document.getElementById("nav-sec");
+  var y = document.getElementById("drive-sec");
+  var z = document.getElementById("spot-sec");
+  var a = document.getElementById("gun-sec");
+  var b = document.getElementById("sec-sec");
+  var c = document.getElementById("logi-sec");
+  var d = document.getElementById("sail-sec");
+  var e = document.getElementById("whale-sec");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+    x.style.display = "none";
+    z.style.display = "none";
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+    e.style.display = "none";
+  } else {
+    y.style.display = "block";
+    x.style.display = "block";
+    z.style.display = "block";
+    a.style.display = "block";
+    b.style.display = "block";
+    c.style.display = "block";
+    d.style.display = "block";
+    e.style.display = "block";
   }
 }
 
